0ddc239e223f7c8cb8a4f48bf6cf847f40af4a41
thesamcode <stores1@digitalmail.one>
2022-07-31 19:28:05 -0700

    new updates

---
+++ first_java_program_practice/Me.java
@@ -0,0 +1,8 @@
+
+public class Me {
+	public static void main(String[] args) {
+        System.out.println("My name is Sam");
+        System.out.println("I am 50 years old");
+        System.out.println("My hometown is San Diego, CA");
+	}
+}
